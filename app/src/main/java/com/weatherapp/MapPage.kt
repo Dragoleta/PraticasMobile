175ada06be65bafb336a4a695e863c0f8da8eff7
jooj <jorgelucasdantas@hotmail.com>
2024-07-28 20:46:54 -0300

    Pratica 02 - Parte 1

--- app/src/main/java/com/weatherapp/MapPage.kt
+++ app/src/main/java/com/weatherapp/MapPage.kt
@@ -1,35 +1,26 @@
 package com.weatherapp
 
-import android.app.Activity
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-@Preview
 @Composable
-fun HomePage() {
+fun MapPage() {
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(colorResource(id = R.color.teal_700))
-            .wrapContentSize(Alignment.Center)
+    modifier = Modifier
+    .fillMaxSize()
+    .background(colorResource(id = R.color.purple_200))
+    .wrapContentSize(Alignment.Center)
     ) {
         Text(
             text = "Home",
@@ -40,4 +31,5 @@ fun HomePage() {
             fontSize = 20.sp
         )
     }
+
 }
