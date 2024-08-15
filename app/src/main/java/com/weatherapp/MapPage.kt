288f41bb2f77b88eb9f6ee508f268cc03df66ab6
jooj <jorgelucasdantas@hotmail.com>
2024-08-15 17:04:20 -0300

    Pratica 04 - Parte 3

--- app/src/main/java/com/weatherapp/MapPage.kt
+++ app/src/main/java/com/weatherapp/MapPage.kt
@@ -1,38 +1,56 @@
 package com.weatherapp
 
 import android.content.Context
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.sp
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
 
 @Composable
 fun MapPage(            modifier: Modifier = Modifier,
                         viewModel: MainViewModel,
                         context: Context
 ) {
-    Column(
-    modifier = Modifier
-    .fillMaxSize()
-    .background(colorResource(id = R.color.purple_200))
-    .wrapContentSize(Alignment.Center)
-    ) {
-        Text(
-            text = "A maps",
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp
+    val recife = LatLng(-8.05, -34.9)
+    val caruaru = LatLng(-8.27, -35.98)
+    val joaopessoa = LatLng(-7.12, -34.84)
+    val camPosState = rememberCameraPositionState ()
+    GoogleMap(modifier = Modifier.fillMaxSize(),
+        onMapClick = { viewModel.add("Nova cidade", location = it) },cameraPositionState = camPosState
+
+        ) {
+        viewModel.cities.forEach {
+            if (it.location != null) {
+                Marker( state = MarkerState(position = it.location!!),
+                    title = it.name, snippet = "${it.location}")
+            }
+        }
+        Marker(
+            state = MarkerState(position = recife),
+            title = "Recife",
+            snippet = "Marcador em Recife",
+            icon = BitmapDescriptorFactory.defaultMarker(
+                BitmapDescriptorFactory.HUE_BLUE
+            )
+        )
+        Marker(
+            state = MarkerState(position = caruaru),
+            title = "Caruaru",
+            snippet = "Marcador em Caruaru",
+            icon = BitmapDescriptorFactory.defaultMarker(
+                BitmapDescriptorFactory.HUE_RED)
+        )
+        Marker(
+            state = MarkerState(position = joaopessoa),
+            title = "Joao Pessoa",
+            icon = BitmapDescriptorFactory.defaultMarker(
+                BitmapDescriptorFactory.HUE_ROSE),
+            snippet = "Marcador em Joao Pessoa"
         )
     }
 
