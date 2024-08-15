f938b3e6b75b4ffc16fba3d74168bfb6d3aea5ec
jooj <jorgelucasdantas@hotmail.com>
2024-08-15 15:30:59 -0300

    Pratica 04 - Parte 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id ("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
 android {
@@ -50,6 +51,12 @@ android {
 }
 
 dependencies {
+
+    // Google maps
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.google.android.gms:play-services-location:21.2.0")
+// Google maps for compose
+    implementation("com.google.maps.android:maps-compose:2.8.0")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
