94dcb981d8a3114dc8bb0474f56a55c0fe04200c
jooj <jorgelucasdantas@hotmail.com>
2024-08-15 17:06:27 -0300

    Pratica 04 - Parte 4

--- app/src/main/java/com/weatherapp/MapPage.kt
+++ app/src/main/java/com/weatherapp/MapPage.kt
@@ -1,27 +1,45 @@
 package com.weatherapp
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
+
 
 @Composable
 fun MapPage(            modifier: Modifier = Modifier,
                         viewModel: MainViewModel,
                         context: Context
 ) {
+    val hasLocationPermission by remember {
+    mutableStateOf(
+        ContextCompat.checkSelfPermission(context,
+            Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED
+    )
+}
+
     val recife = LatLng(-8.05, -34.9)
     val caruaru = LatLng(-8.27, -35.98)
     val joaopessoa = LatLng(-7.12, -34.84)
     val camPosState = rememberCameraPositionState ()
     GoogleMap(modifier = Modifier.fillMaxSize(),
-        onMapClick = { viewModel.add("Nova cidade", location = it) },cameraPositionState = camPosState
+        onMapClick = { viewModel.add("Nova cidade", location = it) },cameraPositionState = camPosState, properties = MapProperties(isMyLocationEnabled = hasLocationPermission),
+        uiSettings = MapUiSettings(myLocationButtonEnabled = true)
 
         ) {
         viewModel.cities.forEach {
